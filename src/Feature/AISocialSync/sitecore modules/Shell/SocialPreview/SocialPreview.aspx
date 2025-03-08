287e861adf80b26fae3fca020a5d137b758fd37b
HI-Kiran <ksoni@horizontal.com>
2025-03-08 18:18:49 +0530

    Preview dialog of social sync

---
+++ src/Feature/AISocialSync/sitecore modules/Shell/SocialPreview/SocialPreview.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Async="true" Language="C#" AutoEventWireup="true" CodeBehind="SocialPreview.aspx.cs" Inherits="AISocialSync.sitecore_modules.Shell.SocialPreview.SocialPreview" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+
+    <title>Social AI</title>
+    <!-- Link Tailwind CSS (CDN) -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet" />
+</head>
+<body class="bg-gray-100">
+
+    <!-- Card Container -->
+    <div class="mx-auto mt-10">
+
+        <!-- Card Content -->
+        <div class="px-6 py-4">
+            <h2 class="font-bold text-xl mb-2">Post description</h2>
+            <p class="text-gray-700 text-base">
+                <asp:Label ID="lblOriginalTextCms" runat="server" Text="Label" CssClass="block text-lg font-medium text-gray-700"></asp:Label>
+            </p>
+        </div>
+
+        <!-- Card Footer -->
+        <div class="px-6 py-4 flex justify-between items-center">
+            <asp:Button CssClass="btnRewrite bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600" ID="btnAIResponse" runat="server" Text="Rewrite with AI!" OnClick="btnAIResponse_Click" />
+            <asp:Button ID="btnPostToTwitter" runat="server" Text="Post to Twitter" CssClass="bg-blue-400 text-white py-2 px-4 rounded-md hover:bg-blue-500" OnClick="PostToTwitter_Click" />
+        </div>
+
+        <asp:Label ID="lblTwitterResponse" runat="server"></asp:Label>
+    </div>
+
+    <!-- Card Container -->
+    <div class="mx-auto mt-10 border-t-4 border-t-blue-500 p-4">
+
+        <!-- Card Content -->
+        <div class="px-6 py-4">
+            <h2 class="font-bold text-xl mb-2">Post description based on Trends</h2>
+            <asp:Label ID="lblTrendsFromCMS" runat="server"></asp:Label>
+            <p class="text-gray-700 text-base">
+                <asp:Label ID="lblRewriteWithTrendingAI" runat="server" CssClass="block text-sm font-medium text-gray-500"></asp:Label>
+            </p>
+        </div>
+
+        <!-- Card Footer -->
+        <div class="px-6 py-4 flex justify-between items-center">
+            <asp:Button CssClass="btnRewrite bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600" ID="btnAITrendResponse" runat="server" Text="Generate with Trend AI!" OnClick="btnAITrendResponse_Click" />
+            <asp:Button ID="btnTrendingPostToTwitter" runat="server" Text="Post to Twitter" CssClass="bg-blue-400 text-white py-2 px-4 rounded-md hover:bg-blue-500" OnClick="TrendingPostToTwitter_Click" />
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ src/Feature/AISocialSync/sitecore modules/Shell/SocialPreview/SocialPreview.aspx.cs
@@ -0,0 +1,76 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Layouts;
+using System;
+using System.Threading.Tasks;
+using HttpMethod = System.Net.Http.HttpMethod;
+using System.Security.Cryptography;
+using System.Net.Http;
+using System.Text;
+using System.Collections.Generic;
+using System.Net;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace AISocialSync.sitecore_modules.Shell.SocialPreview
+{
+    public partial class SocialPreview : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                lblTwitterResponse.Visible = false;
+                var itemId = Request.QueryString["itemId"];
+                if (!string.IsNullOrEmpty(itemId))
+                {
+                    // Get the current item
+                    var database = Sitecore.Configuration.Factory.GetDatabase("master");
+                    Item currentItem = database.GetItem(new ID(itemId));
+
+                    // Set the preview details
+                    if (currentItem != null && IfItemHasPresentation(currentItem))
+                    {
+                        lblOriginalTextCms.Text = currentItem["{A60ACD61-A6DB-4182-8329-C957982CEC74}"];
+
+                        if (currentItem.Fields["SelectTopic"] != null)
+                        {
+                            Sitecore.Data.Fields.MultilistField multiselectField = currentItem.Fields["SelectTopic"];
+                            Sitecore.Data.Items.Item[] items = multiselectField.GetItems();
+
+                            if (items != null && items.Length > 0)
+                            {
+                                lblTrendsFromCMS.Text = string.Join(",", items.Select(x => x.Fields["TrendTitle"]?.Value));
+                            }
+                        }
+                    }
+                    else
+                    {
+                        lblOriginalTextCms.Text = "current item is null";
+                    }
+                }
+            }
+        }
+
+        private bool IfItemHasPresentation(Item item)
+        {
+            // Get the layout definition of the context item
+            LayoutDefinition layoutDefinition = LayoutDefinition.Parse(item.Fields[Sitecore.FieldIDs.LayoutField].Value);
+            // Ensure the layout definition exists
+            if (layoutDefinition != null)
+            {
+                return true;
+            }
+            return false;
+        }
+        protected void PostToTwitter_Click(object sender, EventArgs e)
+        {
+            // Twitter API integration
+            var consumerKey = Sitecore.Configuration.Settings.GetSetting("Twitter.ConsumerKey");
+            var consumerSecret = Sitecore.Configuration.Settings.GetSetting("Twitter.ConsumerSecret");
+            var accessToken = Sitecore.Configuration.Settings.GetSetting("Twitter.AccessToken"); ;
+            var accessTokenSecret = Sitecore.Configuration.Settings.GetSetting("Twitter.AccessTokenSecret");
+            var publishedTweet = PostTweet(lblOriginalTextCms.Text, consumerKey, consumerSecret, accessToken, accessTokenSecret);
+        }
+    }
+}
